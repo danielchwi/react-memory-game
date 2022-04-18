69e447bc3b667e54c92b232ed72689d2b7e43240
danielchwi <daniel.widodo@bitcoin.co.id>
2022-04-18 10:45:54 +0700

    add shuffle and grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import { useState } from 'react'
 import './App.css'
 
+const cardImages = [
+  {"src": "/img/helmet-1.png"},
+  {"src": "/img/potion-1.png"},
+  {"src": "/img/ring-1.png"},
+  {"src": "/img/scroll-1.png"},
+  {"src": "/img/shield-1.png"},
+  {"src": "/img/sword-1.png"},
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffleCard = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+    .map(card => ({...card,  id: Math.random()}))
+    .sort((a,b) => a.id - b.id)
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCard}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map((card) => (
+          <div className='card' key={card.id}>
+            <img calssName="front" src={card.src} alt="front card"/>
+            <img calssName="back" src="/img/cover.png" alt="back card"/>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
